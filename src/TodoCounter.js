aaca3140e0fb7419f71beafa30e7c454267d1450
Luis <luisbohorquezlugo@hotmail.com>
2022-01-03 20:47:25 -0800

    Initial Commit

---
+++ src/TodoCounter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TodoCounter.css';
+
+function TodoCounter() {
+  return (
+    <h2 className="TodoCounter">
+        Welcome to The New World!
+    </h2>
+  );
+}
+
+export { TodoCounter };
