67e8122ebe95d49988f27396453dcde40228a350
Luis <luisbohorquezlugo@hotmail.com>
2022-01-13 10:43:21 -0800

    Add data persistence function with LocalStorage

--- src/App/index.js
+++ src/App/index.js
@@ -6,14 +6,25 @@ import { TodoItem } from '../TodoItem';
 import { CreateTodoButton } from '../CreateTodoButton';
 import './App.css';
 
-const defaulTodos = [
+/*const defaulTodos = [
   { text: 'Ver videos de curso de React', completed: true },
   { text: 'Ver videos de NFT', completed: true },
   { text: 'Socializar', completed: false },
-];
+];*/
 
 function App() {
-  const [todos, setTodos] = React.useState(defaulTodos);
+  const localStorageTodos = localStorage.getItem('TODOS_V1');
+  let parsedTodos = [];
+  
+  if (!localStorageTodos) {
+    localStorage.setItem('TODOS_V1', JSON.stringify([]));
+    parsedTodos = [];
+  } else {
+    parsedTodos = JSON.parse(localStorageTodos);
+  }
+  
+
+  const [todos, setTodos] = React.useState(parsedTodos);
   const [search, setSearch] = React.useState('');
 
   const completedTodos = todos.filter(todo => !!todo.completed).length;
@@ -31,18 +42,25 @@ function App() {
     });
   }
 
+  const saveTodos = (newTodos) => {
+    const stringiedTodos = JSON.stringify(newTodos);
+    localStorage.setItem('TODO_V1', stringiedTodos);
+    setTodos(newTodos);
+  };
+
+
   const completeTodo = (text) => {
     const todoIndex = todos.findIndex(todo => todo.text == text);
     const newTodos = [...todos];    
     newTodos[todoIndex].completed = true;
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
 
   const deleteTodo = (text) => {
     const todoIndex = todos.findIndex(todo => todo.text == text);
     const newTodos = [...todos];    
     newTodos.splice();
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
 
   return (
